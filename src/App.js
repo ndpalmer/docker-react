7a5d15c4e52d103b15a89e18b54610fabfd3712e
ndpalmer <ndpalmer@gmail.com>
2021-06-06 13:24:05 +1000

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>()</code> save to reload.
+          Hello <code>(jnlgkjsengrj)</code> save to reload.
         </p>
         <a
           className="App-link"
